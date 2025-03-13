a396e64d65177d6356786d730ab7617efe11e51d
Terenceb18 <aiden46.tab@gmail.com>
2025-03-13 17:20:30 -0400

    b

--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
+    private Camera _camera;
+    private string CameraName = "Game_Camera";
+    private Vector3 _positionMouse;
+
+
+    private void Start()
+    {
+        _camera = GameObject.Find(CameraName).GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        _positionMouse = _camera.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+
 
 }
+
+
